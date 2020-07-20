e9b8dfa9188d5d8ab58d229ef5031037f1d77094
PorkhunSP <ser.porkhun41@gmail.com>
2020-07-20 19:21:28 +0300

    First Commit Bot Tucker

---
+++ Bot.py
@@ -0,0 +1,45 @@
+from aiogram import Bot, Dispatcher, executor, types
+import Config   # здесь находится токен бота
+import logging
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=Config.token)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+@dp.message_handler(regexp='(^cat[s]?$|puss)')
+async def cats(message: types.Message):
+    with open('.\\data\\cats.jpg', 'rb') as photo:
+        '''
+        # Old fashioned way:
+        await bot.send_photo(
+            message.chat.id,
+            photo,
+            caption='Cats are here 😺',
+            reply_to_message_id=message.message_id,
+        )
+        '''
+
+        await message.reply_photo(photo, caption='Cats are here 😺')
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
